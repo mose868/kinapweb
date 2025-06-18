c592b6e19c354598714d91bb898271fc5b240683
mose868 <moseskimani414@gmail.com>
2025-06-18 15:43:01 +0300

    Remove backend and unwanted files - clean frontend-only project

--- server/routes/contact.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const contactController = require('../controllers/contactController');
-const router = express.Router();
-
-// Public: Submit contact/feedback form
-router.post('/', contactController.submit);
-
-module.exports = router; 
